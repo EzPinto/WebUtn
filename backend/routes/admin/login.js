77f278c031dbbe518e225bfc140aa6cdecdf22e6
EzPinto <ezpinto@frba.utn.edu.ar>
2022-04-09 19:04:56 -0300

    modulo 5

---
+++ backend/routes/admin/login.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var usersModel = require('./../../models/usersModel');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', {
+    layout: "admin/layout"
+  });
+});
+
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: "admin/layout"
+  });
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    var data = await usersModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } // cierro catch
+}); //cierro router.post
+
+module.exports = router;
