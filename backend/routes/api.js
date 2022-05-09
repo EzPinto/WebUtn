764b6306b4b39e72d52472c70ee6f746d5231806
EzPinto <ezpinto@frba.utn.edu.ar>
2022-05-09 20:37:09 -0300

    backend

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var novedadesModel = require("./../models/novedadesModel");
 var cloudinary = require("cloudinary").v2;
+var nodemailer = require("nodemailer");
 
 router.get("/novedades", async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -26,5 +27,35 @@ router.get("/novedades", async function (req, res, next) {
   });
     res.json(novedades);
 });
+
+router.post("/contacto", async (req,res) => {
+    const mail = {
+        to: "ezpinto@frba.utn.edu.ar",
+        subject: "Contacto Web",
+        html: `Se contacto ${req.body.apellido}, ${req.body.nombre} solicitando mas informacion a
+        este correo: ${req.body.email} <br> Ademas, dejo el siguiente mensaje ${req.body.mensaje}. <br>
+        Su telefono es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport ({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    }); //cierra transport
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: "Mensaje enviado"
+    });
+
+}); //cierra post/api
+
+
+
 
 module.exports = router;
