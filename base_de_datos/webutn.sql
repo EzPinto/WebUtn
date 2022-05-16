8762c7a05b202127fb311b4281fee14dec6909df
EzPinto <ezpinto@frba.utn.edu.ar>
2022-05-16 12:27:22 -0300

    Entrega Final

--- base_de_datos/webutn.sql
+++ base_de_datos/webutn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 03-05-2022 a las 14:28:40
+-- Tiempo de generación: 16-05-2022 a las 15:26:28
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `cuerpo` text NOT NULL,
   `img_id` char(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `news`
@@ -44,9 +44,9 @@ CREATE TABLE IF NOT EXISTS `news` (
 INSERT INTO `news` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
 (1, 'Doná sangre de forma segura - Es rápido e indoloro.', 'Próximas campañas voluntarias de donación de sangre con turnos programados. También en hospitales.', 'Próximas campañas de donación voluntaria de sangre, abiertas a la comunidad planificadas por la Red de Medicina Transfusional: \r\n\r\n- Domingo 3 de abril	Proyecto Damos (Juventud) en Fund. Hillel, O\'Higgins 1560	10.00 a 15.30 hs.\r\n\r\n-Martes 5 de abril	Escuela Prof. J. Weitzman, Varela 850	8.00 a 15.00 hs.	BA Salud y Comunidad Escuela Weitzman\r\n\r\n-Viernes 8 de abril	Hall Central de la Facultad de Ciencias Médicas (UBA), Paraguay 2155	9.30 a 15.00 hs.	BA Salud, Extensión Universitaria, UBA en Acción, UBA Medicina y CECIM', NULL),
 (2, 'Sangre de forma segura - Es rápido e indoloro.', 'Próximas campañas voluntarias de donación de sangre con turnos programados. También en hospitales.', 'Próximas campañas de donación voluntaria de sangre, abiertas a la comunidad planificadas por la Red de Medicina Transfusional: \r\n\r\n- Domingo 3 de abril	Proyecto Damos (Juventud) en Fund. Hillel, O\'Higgins 1560	10.00 a 15.30 hs.\r\n\r\n-Martes 5 de abril	Escuela Prof. J. Weitzman, Varela 850	8.00 a 15.00 hs.	BA Salud y Comunidad Escuela Weitzman\r\n\r\n-Viernes 8 de abril	Hall Central de la Facultad de Ciencias Médicas (UBA), Paraguay 2155	9.30 a 15.00 hs.	BA Salud, Extensión Universitaria, UBA en Acción, UBA Medicina y CECIM', ''),
-(3, 'Posta de Donacion 31/08', 'Hospital Naval', '12312', ''),
-(6, 'Posta de Donacion 31/05', 'Hospital de clinicas', 'asdasdasasdasdasasdasdasasdasdasasdasdasasdasdasasdasdas', 'ywdlbsyyhztu6upadkrn'),
-(8, 'Posta de Donacion 24/07', 'Hospital Garrahan', 'asdasdasdasssssssssssssssssssssssssdasdasdas', 'ggqge80ujo0wahxtgukq');
+(3, 'Posta de Donacion 31/08', 'Ubicación: Facultad de Economía', 'El próximo 31 de Agosto, estaremos realizando una colecta de sangre, en el Hall principal de la Facultad de Economía; Ubicada en la calle Av. Cordoba 2122, CABA.\r\nSi quieren acercarse, solo basta venir con DNI, ser mayor de 18 años; y haber desayuno.', 'srukbl7palx3kwqcob8k'),
+(6, 'Posta de Donacion 15/08', 'Ubicación: Facultad de Medicina', 'El proximo 15 de Agosto, estaremos realizando una colecta de sangre, en el Hall principal de la Facultado de Medicina; Ubicada en la calle Paraguay 2155, CABA.\r\nSi quieren acercarse, solo basta venir con DNI, ser mayor de 18 años; y haber desayuno.', 'nmaabqx4wngbwigkee63'),
+(8, 'Solicitud de donantes para el 24/07', 'Ubicación: Hospital Garrahan', 'El proximo 24 de julio, estamos realizando un llamado para la obtención de dadores de sangre, para reabastecer el banco.', 'rtsdcvkj5b7ynahfiabn');
 
 -- --------------------------------------------------------
 
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `usuario` varchar(60) NOT NULL,
   `password` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `users`
@@ -68,7 +68,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `usuario`, `password`) VALUES
 (1, 'ezequiel', '81dc9bdb52d04dc20036dbd8313ed055'),
-(2, 'florencia', '81dc9bdb52d04dc20036dbd8313ed055');
+(2, 'florencia', '81dc9bdb52d04dc20036dbd8313ed055'),
+(3, 'eze', '1234');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
